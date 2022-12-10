41b7561b64a0f703b6257a1065509e1c0ee2f04e
Vu Anh Lam <vuanhlam000@gmail.com>
2022-12-10 15:40:32 +0700

    parent and child component

--- src/components/Mycomponent.js
+++ src/components/Mycomponent.js
@@ -1,54 +1,17 @@
 //giai đoạn đầu khóa học sẽ dùng class component
 
 import React from "react";
+import UsersInfo from "./UsersInfo";
 
 class Mycomponent extends React.Component {
 
-    // JSX cho phép viết code javaScript trong code của html
-    // điểm hạn chế JSX chỉ return về được một element duy nhất 
-    // muốn code js để tạo ra nội dung reder động thì code trong dấu {.. your code..}
-
-    state = {
-        name:  'lam',
-        address: 'ha noi',
-        age: 29
-    };
-
-    handleClick = (event) => {
-        
-        // merge state => react class
-
-        this.setState({
-            name: 'Vu',
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-
-    }
-
-    handleOnMouseOver = (event) => {
-        console.log(event);
-    }
-
-    handleOnchangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-    }
-
-    handleOnsubmit = (event) => {
-        event.preventDefault();
-    }
+    
 
     render() { // tập hợp các khối html và render ra giao diện
         return (
             <div>
-                <p>my name is {this.state.name}</p>
-                <p>my address is {this.state.address}</p>
-                <p>my age is {this.state.age}</p>
-                <form onSubmit={(event) => this.handleOnsubmit(event)}> 
-                    <input onChange={(event) => this.handleOnchangeInput(event)} type="text"/>
-                    <button>submit</button>
-                </form>
+                
+                <UsersInfo/>
             </div>
         );
 
---
+++ src/components/UsersInfo.js
@@ -0,0 +1,81 @@
+import React from "react";
+import Mycomponent from "./Mycomponent";
+
+
+class UsersInfo extends React.Component {
+
+    // JSX cho phép viết code javaScript trong code của html
+    // điểm hạn chế JSX chỉ return về được một element duy nhất 
+    // muốn code js để tạo ra nội dung reder động thì code trong dấu {.. your code..}
+
+    state = {
+        name:  'lam',
+        address: 'ha noi',
+        age: 29
+    };
+
+    handleClick = (event) => {
+        
+        // merge state => react class
+
+        this.setState({
+            name: 'Vu',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+
+    }
+
+    handleOnMouseOver = (event) => {
+        console.log(event);
+    }
+
+    handleOnchangeInput = (event) => {
+
+        //bad code
+        // this.state.name = event.target.value;
+
+        //good
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleOnchangeAge = (event) => {
+        this.setState({
+            age: event.target.value
+        })
+    }
+
+    handleOnsubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+    
+    render() {
+        return (
+            <div>
+                <p>my name is {this.state.name}</p>
+                <p>my address is {this.state.address}</p>
+                <p>my age is {this.state.age}</p>
+                <form onSubmit={(event) => this.handleOnsubmit(event)}> 
+                    <label>your name: </label>
+                    <input 
+                        onChange={(event) => this.handleOnchangeInput(event)} 
+                        type="text"
+                        value={this.state.name}
+                    />
+                    <br></br>                        
+                    <label>your age: </label>
+                    <input 
+                        onChange={(event) => this.handleOnchangeAge(event)} 
+                        type="text"
+                        value={this.state.age}
+                    />
+                    <button>submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default UsersInfo
